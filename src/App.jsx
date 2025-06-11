4bf2f84c2a82cef1b586d9be59752f5d51b630e9
Aniket Rana <aniketrana7681@gmail.com>
2025-06-11 13:52:10 +0530

    started

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Body from './components/Body'
+
+// import './App.css'
+
+function App() {
+  
+
+  return (
+   <div >
+    <Body/>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div >
+        <Header/>
+        <img 
+        className=''
+        src='https://assets.nflxext.com/ffe/siteui/vlv3/6863f6e8-d419-414d-b5b9-7ef657e67ce4/web/IN-en-20250602-TRIFECTA-perspective_27a3fdfa-126f-4148-b153-55d60b51be6a_small.jpg'/>
+            
+    </div>
+  )
+}
+
+export default Login
